d1a1e713d4a71dfdf06613be0a4b8584dbb7ce8e
ahmed belal <legendb@gmail.com>
2025-02-04 23:02:29 +0200

    add more validaition in auth moduel

--- TrelloCopy/Features/AuthManagement/RegisterUser/RegisterUserRequestViewModel.cs
+++ TrelloCopy/Features/AuthManagement/RegisterUser/RegisterUserRequestViewModel.cs
@@ -8,22 +8,29 @@ public class RegisterUserRequestViewModelValidator : AbstractValidator<RegisterU
 {
     public RegisterUserRequestViewModelValidator()
     {
-        // RuleFor(x => x.Email)
-        //     .NotEmpty().WithMessage("Email is required.")
-        //     .EmailAddress().WithMessage("Please provide a valid email address.");
-        //
-        // RuleFor(x => x.Password)
-        //     .NotEmpty().WithMessage("Password is required.")
-        //     .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
-        //
-        // RuleFor(x => x.Name)
-        //     .NotEmpty().WithMessage("Name is required.");
-        //
-        // RuleFor(x => x.PhoneNo)
-        //     .NotEmpty().WithMessage("Phone number is required.")
-        //     .Matches(@"^\+?\d{10,15}$").WithMessage("Please provide a valid phone number.");
-        //
-        // RuleFor(x => x.Country)
-        //     .NotEmpty().WithMessage("Country is required.");
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Please provide a valid email address.")
+            .Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+            .WithMessage("Please enter a correctly formatted email address.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches(@"\d").WithMessage("Password must contain at least one number.")
+            .Matches(@"[\W_]").WithMessage("Password must contain at least one special character (!@#$%^&* etc.).");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.");
+
+        RuleFor(x => x.PhoneNo)
+            .NotEmpty().WithMessage("Phone number is required.")
+            .Matches(@"^\+?\d{10,15}$").WithMessage("Please provide a valid phone number.");
+
+        RuleFor(x => x.Country)
+            .NotEmpty().WithMessage("Country is required.");
     }
 }
+
